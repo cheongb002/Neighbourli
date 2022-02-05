f70211974967cef6a10154680ec78a65d39f1d25
andy-zhang201 <andrewzhang1000@gmail.com>
2022-02-05 13:23:22 -0500

    New link in navbar

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -15,7 +15,7 @@ export default function Feed() {
                 <Nav.Link href="/home">Home</Nav.Link>
                 <Nav.Link href="/feed">Feed</Nav.Link>
                 <Nav.Link href="/">Login</Nav.Link>
-                
+                <Nav.Link href="/create_event">Create Event</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -33,19 +33,17 @@ function Homepage() {
             </Navbar.Collapse>
         </Container>
         </Navbar>
-        <p>
+        {/* <p>
             This is the homepage of our website. To see our feed, go <a href="/feed">here.</a> To see our login, go <a href="/">here.</a>
         </p>
         <p>
             Lorem Ipsum.
-        </p>
+        </p> */}
 
         {/* <div class="container2"> */}
 
         <div class="myDiv">
-            <p>Hello World</p>
-            <img src="dota.png" alt="Dota2 is the best" width="500" height="600"></img>
-            <br></br>
+            <p>This is the homepage of our website.</p>
             <Figure>
                 <Figure.Image
                     width={171}
@@ -57,6 +55,7 @@ function Homepage() {
                     Nulla vitae elit libero, a pharetra augue mollis interdum.
                 </Figure.Caption>
             </Figure>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         {/* </div> */}
     </div>
